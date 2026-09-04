23ab69562fe39d41bb4fbe7d420dc134c3f8d314
deeptechnicalgo-sudo <deep.technical.go@gmail.com>
2026-09-04 13:06:11 +0300

    Integrate Google Analytics tracking script
    Added Google Analytics tracking script to the layout.

--- src/app/layout.js
+++ src/app/layout.js
@@ -36,6 +36,22 @@ export default function RootLayout({ children }) {
       <SessionProvider>
         <DataProvider>
           <DataAProvider>
+            <head>
+     <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-JVM4SDFJZK"
+        />
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-JVM4SDFJZK');
+          `}
+        </Script>
+        
+        </head>
             <body>
               {children}
 
