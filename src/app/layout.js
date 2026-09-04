84d3f1209f24d7da16cab8bc3d9dd3a886d10d67
deeptechnicalgo-sudo <deep.technical.go@gmail.com>
2026-09-04 20:25:33 +0300

    Monetag 6
    Ad notefiction and push 6

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,7 +15,6 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-
 export const metadata = {
   title: "PrayerSync",
   description: "Put your prayer time to your Outlook or Google Calendar",
@@ -25,8 +24,6 @@ export const metadata = {
   },
 };
 
-
-
 export default function RootLayout({ children }) {
   return (
     <html
@@ -34,68 +31,74 @@ export default function RootLayout({ children }) {
       className={`${geistSans.variable} ${geistMono.variable}`}
       suppressHydrationWarning
     >
-      <SessionProvider>
-        <DataProvider>
-          <DataAProvider>
-            <head>
+      <head>
 
-              <script>
-                {(function (s) {
-                  s.dataset.zone = "11726998";
-                  s.src = "https://nap5k.com/tag.min.js";
-                })(
-                  [document.documentElement, document.body]
-                    .filter(Boolean)
-                    .pop()
-                    .appendChild(document.createElement("script"))
-                )}
-              </script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(s){
+                s.dataset.zone='11726998',
+                s.src='https://nap5k.com/tag.min.js'
+              })([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))
+            `,
+          }}
+        />
 
-              <script>
-                {(function (s) {
-                  s.dataset.zone = "11726834";
-                  s.src = "https://al5sm.com/tag.min.js";
-                })(
-                  [document.documentElement, document.body]
-                    .filter(Boolean)
-                    .pop()
-                    .appendChild(document.createElement("script"))
-                )}
-              </script>
 
-              <Script src="https://5gvci.com/act/files/tag.min.js?z=11726628" data-cfasync="false" async />
-              <Script
-                async
-                src="https://www.googletagmanager.com/gtag/js?id=G-JVM4SDFJZK"
-              />
-              <Script id="google-analytics">
-                {`
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(s){
+                s.dataset.zone='11726834',
+                s.src='https://al5sm.com/tag.min.js'
+              })([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))
+            `,
+          }}
+        />
+
+
+        <Script
+          id="monetag-zone-11726628"
+          src="https://5gvci.com/act/files/tag.min.js?z=11726628"
+          data-cfasync="false"
+          strategy="afterInteractive"
+        />
+
+
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-JVM4SDFJZK"
+        />
+
+        <Script id="google-analytics">
+          {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-
             gtag('config', 'G-JVM4SDFJZK');
           `}
-              </Script>
+        </Script>
+      </head>
 
-            </head>
-            <body>
+      <body>
+        <SessionProvider>
+          <DataProvider>
+            <DataAProvider>
               {children}
+            </DataAProvider>
+          </DataProvider>
+        </SessionProvider>
 
 
-              <Script
-                async
-                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8383999192768244"
-                crossOrigin="anonymous"
-                strategy="afterInteractive"
-              />
-            </body>
-          </DataAProvider>
-        </DataProvider>
-      </SessionProvider>
-
-
-
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8383999192768244"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+      </body>
     </html>
   );
 }
+
+
